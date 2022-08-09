e586e49978c5bbc2fecb591ef4e4de9639fb3e16
Michael Johann <michaeljohann94@outlook.com>
2022-08-09 21:23:30 +0200

    initial commit

---
+++ host_gen/__main__.py
@@ -0,0 +1,54 @@
+import os.path
+from glob import iglob
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+TARGET = os.path.abspath(os.path.join(os.getcwd(), "10-adbhosts.txt"))
+CACHE = set()
+WL_CACHE = set()
+ITERATOR = iglob("./data/blocklist/**.txt")
+WL_ITERATOR = iglob("./data/whitelist/**.txt")
+
+
+def make_host(url):
+    return "0.0.0.0 {url}".format(url=url)
+
+
+def make_cache():
+    logger.info("Caching blacklists")
+    for entry in ITERATOR:
+        with open(entry, "r") as fin:
+            for url in fin.readlines():
+                if url[0] == "#":
+                    continue
+                logger.debug("Adding {} to blacklist".format(url))
+                CACHE.add(make_host(url))
+
+
+def make_host_file():
+    with open(TARGET, "w") as fout:
+        logger.info("Writing host file")
+        fout.writelines((host.strip() for host in CACHE.difference(WL_CACHE)))
+
+
+def make_whitelist():
+    logger.info("Caching whitelists")
+    for entry in WL_ITERATOR:
+        with open(entry, "r") as fin:
+            for url in fin.readlines():
+                if url[0] == "#":
+                    continue
+                logger.debug("Adding {} to whitelist".format(url))
+                WL_CACHE.add(make_host(url))
+
+
+def main():
+    make_cache()
+    make_whitelist()
+    make_host_file()
+
+
+if __name__ == "__main__":
+    main()
